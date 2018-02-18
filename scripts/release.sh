b163d236709d98111f200784ee09bf4c1cf08093
tsuyoshiwada <wadackel@gmail.com>
2018-02-18 23:13:07 +0900

    chore: Fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  gox -os="darwin linux windows" -arch="amd64 386" -output="$(pwd)/dist/{{.Dir}}_{{.OS}}_{{.Arch}}" ./cmd/git-chglog
+  echo "Start release ..."
+  gox -os="darwin linux windows" -arch="amd64 386" -output="$REPO_ROOT/dist/{{.Dir}}_{{.OS}}_{{.Arch}}" ./cmd/git-chglog
   ghr --username git-chglog --token $GITHUB_TOKEN --replace `grep 'Version =' ./cmd/git-chglog/version.go | sed -E 's/.*"(.+)"$$/\1/'` dist/
+  echo "Released!"
+else
+  echo "Skip release"
 fi
