b01be882302ad42915ea4462ebc895cd76b4a1f8
tsuyoshiwada <wadackel@gmail.com>
2018-02-24 00:45:52 +0900

    fix: Fix error message when `Tag` can not be acquired

--- chglog_test.go
+++ chglog_test.go
@@ -97,7 +97,61 @@ func TestGeneratorNotFoundCommits(t *testing.T) {
 
 	buf := &bytes.Buffer{}
 	err := gen.Generate(buf, "foo")
+	assert.Error(err)
+	assert.Equal("", buf.String())
+}
+
+func TestGeneratorNotFoundCommitsOne(t *testing.T) {
+	assert := assert.New(t)
+	testName := "not_found"
+
+	setup(testName, func(git gitcmd.Client) {
+		git.Exec("commit", "--allow-empty", "--date", "Mon Jan 1 00:00:00 2018 +0000", "-m", "chore(*): First commit")
+	})
+
+	gen := NewGenerator(&Config{
+		Bin:        "git",
+		WorkingDir: filepath.Join(testRepoRoot, testName),
+		Template:   filepath.Join(cwd, "testdata", testName+".md"),
+		Info: &Info{
+			RepositoryURL: "https://github.com/git-chglog/git-chglog",
+		},
+		Options: &Options{
+			CommitFilters:        map[string][]string{},
+			CommitSortBy:         "Scope",
+			CommitGroupBy:        "Type",
+			CommitGroupSortBy:    "Title",
+			CommitGroupTitleMaps: map[string]string{},
+			HeaderPattern:        "^(\\w*)(?:\\(([\\w\\$\\.\\-\\*\\s]*)\\))?\\:\\s(.*)$",
+			HeaderPatternMaps: []string{
+				"Type",
+				"Scope",
+				"Subject",
+			},
+			IssuePrefix: []string{
+				"#",
+				"gh-",
+			},
+			RefActions:   []string{},
+			MergePattern: "^Merge pull request #(\\d+) from (.*)$",
+			MergePatternMaps: []string{
+				"Ref",
+				"Source",
+			},
+			RevertPattern: "^Revert \"([\\s\\S]*)\"$",
+			RevertPatternMaps: []string{
+				"Header",
+			},
+			NoteKeywords: []string{
+				"BREAKING CHANGE",
+			},
+		},
+	})
+
+	buf := &bytes.Buffer{}
+	err := gen.Generate(buf, "foo")
 	assert.Contains(err.Error(), "\"foo\" was not found")
+	assert.Error(err)
 	assert.Equal("", buf.String())
 }
 
