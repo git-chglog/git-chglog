fd21402f6677306f28f68a3defd27f69fc766b3f
Reda Khaled <rkhaled.pro@outlook.com>
2022-12-08 08:42:23 +0000

    feat: support jira personal access token

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/AlecAivazis/survey/v2 v2.3.6
 	github.com/Masterminds/sprig/v3 v3.2.3
 	github.com/andygrunwald/go-jira v1.16.0
+	github.com/andygrunwald/go-jira/v2 v2.0.0-20231114185916-57d1e28f1bb7
 	github.com/coreos/go-semver v0.3.1
 	github.com/fatih/color v1.15.0
 	github.com/imdario/mergo v0.3.13
@@ -23,7 +24,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
-	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/huandu/xstrings v1.3.3 // indirect
@@ -41,7 +42,7 @@ require (
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/term v0.2.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- jira.go
+++ jira.go
@@ -1,7 +1,10 @@
 package chglog
 
 import (
+	"fmt"
+
 	agjira "github.com/andygrunwald/go-jira"
+	agjirav2 "github.com/andygrunwald/go-jira/v2/onpremise"
 )
 
 // JiraClient is an HTTP client for Jira
@@ -10,28 +13,41 @@ type JiraClient interface {
 }
 
 type jiraClient struct {
-	username string
-	token    string
-	url      string
+	username    string
+	token       string
+	bearerToken string
+	url         string
 }
 
 // NewJiraClient returns an instance of JiraClient
 func NewJiraClient(config *Config) JiraClient {
 	return jiraClient{
-		username: config.Options.JiraUsername,
-		token:    config.Options.JiraToken,
-		url:      config.Options.JiraURL,
+		username:    config.Options.JiraUsername,
+		token:       config.Options.JiraToken,
+		bearerToken: config.Options.JiraBearerToken,
+		url:         config.Options.JiraURL,
 	}
 }
 
-func (jira jiraClient) GetJiraIssue(id string) (*agjira.Issue, error) {
+func (jira jiraClient) client() (*agjira.Client, error) {
+	if jira.bearerToken != "" {
+		tp := agjirav2.BearerAuthTransport{
+			Token: jira.bearerToken,
+		}
+		return agjira.NewClient(tp.Client(), jira.url)
+	}
+
 	tp := agjira.BasicAuthTransport{
 		Username: jira.username,
 		Password: jira.token,
 	}
-	client, err := agjira.NewClient(tp.Client(), jira.url)
+	return agjira.NewClient(tp.Client(), jira.url)
+}
+
+func (jira jiraClient) GetJiraIssue(id string) (*agjira.Issue, error) {
+	client, err := jira.client()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("cannot instantiate jira client, got:%v", err)
 	}
 	issue, _, err := client.Issue.Get(id, nil)
 	return issue, err
