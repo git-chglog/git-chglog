d199862b02bdcee4b8c6498fe39781d70c9a9a7b
David Alpert <david@spinthemoose.com>
2023-03-28 13:13:22 -0500

    feat: when tags are not found return commits as Unreleased

--- chglog.go
+++ chglog.go
@@ -153,8 +153,8 @@ func (gen *Generator) Generate(w io.Writer, query string) error {
 		return err
 	}
 
-	if len(versions) == 0 {
-		return fmt.Errorf("commits corresponding to \"%s\" was not found", query)
+	if len(versions) == 0 && len(unreleased.Commits) == 0 {
+		return fmt.Errorf("commits corresponding to \"%s\" were not found", query)
 	}
 
 	return gen.render(w, unreleased, versions)
@@ -163,7 +163,12 @@ func (gen *Generator) Generate(w io.Writer, query string) error {
 func (gen *Generator) buildVersionsFromRefs(query string) (*Unreleased, []*Version, error) {
 	tags, first, err := gen.getTags(query)
 	if err != nil {
-		return nil, nil, err
+		unreleased, err2 := gen.generateUnreleasedForRevList(query)
+		if err2 != nil {
+			return nil, nil, fmt.Errorf("%s and querying unreleased commits by '%s' returned %w", err, query, err2)
+		}
+		versions := make([]*Version, 0)
+		return unreleased, versions, nil
 	}
 
 	unreleased, err := gen.readUnreleased(tags)
